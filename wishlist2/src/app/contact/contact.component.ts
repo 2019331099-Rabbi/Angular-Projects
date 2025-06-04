a45996743f841cde073b5599422600f4867e5592
2019331099-Rabbi <shahedrabbi955@gmail.com>
2025-06-04 17:38:06 +0600

    Reactive Forms

--- wishlist2/src/app/contact/contact.component.ts
+++ wishlist2/src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from './invalidEmailDomain';
+
+const invalidEmailDomain = createInvalidDomainValidator(['gmail.com', 'yahoo.com']);
 
 @Component({
   selector: 'app-contact',
@@ -11,11 +14,11 @@ export class ContactComponent {
   contactForm = new FormGroup({
     senderName: new FormControl(
       '',
-      Validators.required
+      [Validators.required, Validators.minLength(3)]
     ),
     senderEmail: new FormControl(
       '',
-      [Validators.required, Validators.email]
+      [Validators.required, Validators.email, invalidEmailDomain]
     ),
     senderMessage: new FormControl(
       '',
